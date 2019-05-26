5de2c28d2282c0a50269ba2d1ed08e143dbf619e
childers <joshua9906@hotmail.com>
2019-05-26 00:56:47 -0400

    Cleaned up Memory.h

--- src/Memory.h
+++ src/Memory.h
@@ -32,29 +32,28 @@ class APU;
 class Memory
 {
 public:
-	Memory(std::shared_ptr<spdlog::logger> logger,
+    Memory(std::shared_ptr<spdlog::logger> logger,
         std::shared_ptr<CartridgeReader> cartidgeReader,
         std::shared_ptr<MBC> mbc,
         std::shared_ptr<GPU> gpu,
         std::shared_ptr<Joypad> joypad,
         std::shared_ptr<APU> apu);
-	virtual ~Memory();
+    virtual ~Memory();
     Memory& operator=(const Memory& rhs);
 
     void reset();
-	void initWorkRAM(bool isColorGB);
-	void initROMBanks();
-	void do_oam_dma_transfer(uint8_t start_address);
+    void initWorkRAM(bool isColorGB);
+    void initROMBanks();
+    void do_oam_dma_transfer(uint8_t start_address);
     void do_cgb_oam_dma_transfer(uint8_t & hdma1, uint8_t & hdma2, uint8_t & hdma3, uint8_t & hdma4, uint8_t & hdma5);
     void do_cgb_h_blank_dma(uint8_t & hdma1, uint8_t & hdma2, uint8_t & hdma3, uint8_t & hdma4, uint8_t & hdma5);
     void writeToTimerRegisters(uint16_t addr, uint8_t val);
-	void updateTimer(const uint8_t & ticks, const uint32_t & clock_speed);
-
-	uint8_t readByte(uint16_t pos, bool limit_access = true);
-	void setByte(uint16_t pos, uint8_t val, bool limit_access = true);
-
+    void updateTimer(const uint8_t & ticks, const uint32_t & clock_speed);
     void initGBPowerOn();
+    void setByte(uint16_t pos, uint8_t val, bool limit_access = true);
+    uint8_t readByte(uint16_t pos, bool limit_access = true);
 
+    // Variables
     std::shared_ptr<CartridgeReader> cartridgeReader;
     std::shared_ptr<MBC> mbc;
     std::shared_ptr<GPU> gpu;
@@ -65,25 +64,25 @@ public:
     unsigned char cgb_speed_mode;
     unsigned char cgb_undoc_reg_ff6c;
     unsigned char cgb_undoc_regs[0xFF77 - 0xFF72];
-	unsigned char high_ram[0x7F];
-	unsigned char gamepad;
-	unsigned char timer[0x04];
-	unsigned char linkport[3];
-	bool firstTen = false;
-	std::string blargg = "";
+    unsigned char high_ram[0x7F];
+    unsigned char gamepad;
+    unsigned char timer[0x04];
+    unsigned char linkport[3];
+    bool firstTen = false;
+    std::string blargg = "";
 
-	unsigned char interrupt_flag;
-	unsigned char interrupt_enable;
+    unsigned char interrupt_flag;
+    unsigned char interrupt_enable;
 
-	int num_working_ram_banks;
-	int curr_working_ram_bank;
-	bool is_color_gb;
-	std::vector<std::vector<unsigned char>> working_ram_banks;
-	std::shared_ptr<spdlog::logger> logger;
+    int num_working_ram_banks;
+    int curr_working_ram_bank;
+    bool is_color_gb;
+    std::vector<std::vector<unsigned char>> working_ram_banks;
+    std::shared_ptr<spdlog::logger> logger;
 
-	// Timer variables
-	bool timer_enabled;
-	uint32_t clock_frequency;
+    // Timer variables
+    bool timer_enabled;
+    uint32_t clock_frequency;
 
     uint32_t clock_div_rate;
     uint32_t clock_tima_rate;
